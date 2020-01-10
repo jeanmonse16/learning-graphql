752e450236d343ed0e7929d5302907f5c5f0714d
jeanpier <jeanpier.dev@gmail.com>
2020-01-09 23:31:29 -0400

    terminando graphql

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const { makeExecutableSchema } = require('graphql-tools')
 const expressGql = require('express-graphql')
 const { readFileSync } = require('fs')
@@ -12,9 +13,12 @@ const app = express()
 const port = process.env.PORT || 3000
 
 // Definiendo y creano el schema de la API  de graphQL
-const typeDefs = readFileSync( join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
+const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
 
-const schema = makeExecutableSchema({ typeDefs, resolvers})
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+const isDev = process.env.NODE_ENV !== 'production' 
+
+app.use(cors())
 
 // Los métodos que realizaran los request para hacer queries a la API son los resolvers
 
@@ -22,7 +26,7 @@ const schema = makeExecutableSchema({ typeDefs, resolvers})
 app.use('/api', expressGql({
   schema: schema,
   rootValue: resolvers,
-  graphiql: true
+  graphiql: isDev
 }))
 
 app.listen(port, () => console.log('server listening on ' + port))
